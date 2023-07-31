80d23eb836bbc9fa07835a1f931fd676991ca7d0
mustafa <mustafamcm9@gmail.com>
2023-07-31 14:24:02 +0300

    add user  registration handling

--- RecipeShare/base/views.py
+++ RecipeShare/base/views.py
@@ -1,7 +1,28 @@
 from django.shortcuts import render, redirect
+from .forms import UserForm
+from django.contrib import messages
+from django.contrib.auth.models import User
 
 # Create your views here.
 
 def home(request):
-    context = { 'title': 'home' } # add contenxt later
+    context = {  } # add contenxt later
     return render(request, 'base/home.html', context)
+
+def sign_up(request):
+    form = UserForm().as_p
+    context = { 'form': form }
+    
+    if request.method == 'POST':
+        # handle POST request
+        crd = request.POST # credentials 
+        try:
+            user = User.objects.create_user(crd['username'], None, crd['password'])
+        except:
+            messages.warning(request, 'error while creating user')
+
+        # user is created and saved to the database at this point.
+        return redirect('home')
+
+
+    return render(request, 'form.html', context)
