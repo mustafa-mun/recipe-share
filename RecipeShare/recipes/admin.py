e0bfadcc6e3a8e5751399aa66a726ca39221d886
mustafa <mustafamcm9@gmail.com>
2023-08-01 22:02:35 +0300

    register models

--- RecipeShare/recipes/admin.py
+++ RecipeShare/recipes/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Recipe, MainIngredient, RecipeMainIngredient, Ingredient, RecipeIngredient
+from .models import Recipe, MainIngredient, RecipeMainIngredient, Ingredient, RecipeIngredient, PrepTime, Cuisine, RecipePrepTimes, RecipeCuisine, Type, RecipeTypes
 # Register your models here.
 
 
@@ -8,3 +8,9 @@ admin.site.register(MainIngredient)
 admin.site.register(RecipeIngredient)
 admin.site.register(RecipeMainIngredient)
 admin.site.register(Ingredient)
+admin.site.register(PrepTime)
+admin.site.register(RecipePrepTimes)
+admin.site.register(Cuisine)
+admin.site.register(RecipeCuisine)
+admin.site.register(Type)
+admin.site.register(RecipeTypes)
