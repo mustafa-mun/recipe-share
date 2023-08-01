011b13945287516061fa19e2a0fcc536cc0198a4
mustafa <mustafamcm9@gmail.com>
2023-08-02 00:45:53 +0300

    change default image

--- RecipeShare/recipes/models.py
+++ RecipeShare/recipes/models.py
@@ -14,7 +14,7 @@ class Recipe(models.Model):
   recipe_name = models.CharField(max_length=150)
   recipe_description = models.TextField(null=True, blank=True)
   recipe_method = models.TextField(default="")
-  recipe_image = models.TextField(default="https://www.google.com/url?sa=i&url=http%3A%2F%2Fthehalalworld.com%2Ftr%2Fsardine-tea-sandwiches%3Flang%3Dtr&psig=AOvVaw33Fv5gwco6WgwnDG5XGbT1&ust=1690974146425000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCODsrtynu4ADFQAAAAAdAAAAABAE")
+  recipe_image = models.TextField(default="https://thehalalworld.com/uploads/pages/Sardine-tea-sandwiches.jpg")
   updated_at = models.DateTimeField(auto_now=True)
   created_at = models.DateTimeField(auto_now_add=True)
     
