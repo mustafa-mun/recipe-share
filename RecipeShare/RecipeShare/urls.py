614e1e50a83c6614d6c5b01d4a9363f965e3bb15
mustafa <mustafamcm9@gmail.com>
2023-08-01 21:11:14 +0300

    create recipes app

--- RecipeShare/RecipeShare/urls.py
+++ RecipeShare/RecipeShare/urls.py
@@ -18,6 +18,8 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('', include('base.urls')),
+    path('recipes/', include('recipes.urls')),
     path('admin/', admin.site.urls),
-    path('', include('base.urls'))
+    
 ]
---
+++ RecipeShare/recipes/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from django.contrib.auth.models import User
+import uuid
+
+# Create your models here.
+
+class Recipe(models.Model):
+  id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+  recipe_name = models.CharField(max_length=150)
+  recipe_description = models.TextField(null=True, blank=True)
+  recipe_image = models.TextField(default="https://www.google.com/url?sa=i&url=http%3A%2F%2Fthehalalworld.com%2Ftr%2Fsardine-tea-sandwiches%3Flang%3Dtr&psig=AOvVaw33Fv5gwco6WgwnDG5XGbT1&ust=1690974146425000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCODsrtynu4ADFQAAAAAdAAAAABAE")
+  updated_at = models.DateTimeField(auto_now=True)
+  created_at = models.DateTimeField(auto_now_add=True)
+    
+  def __str__(self) -> str:
+    return self.recipe_name
