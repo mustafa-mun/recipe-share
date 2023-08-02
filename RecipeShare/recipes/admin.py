3a55d7734f0f1a2b0875c1bb1c140602e12f1ae8
mustafa <mustafamcm9@gmail.com>
2023-08-02 17:11:43 +0300

    remove ingredient table   Ingredients will be stored as text, Main ingredient table will handle the ingredient-based searchs.

--- RecipeShare/recipes/admin.py
+++ RecipeShare/recipes/admin.py
@@ -1,13 +1,11 @@
 from django.contrib import admin
-from .models import Recipe, MainIngredient, RecipeMainIngredient, Ingredient, RecipeIngredient, PrepTime, Cuisine, RecipePrepTimes, RecipeCuisine, Type, RecipeTypes
+from .models import Recipe, MainIngredient, RecipeMainIngredient, PrepTime, Cuisine, RecipePrepTimes, RecipeCuisine, Type, RecipeTypes
 # Register your models here.
 
 
 admin.site.register(Recipe)
 admin.site.register(MainIngredient)
-admin.site.register(RecipeIngredient)
 admin.site.register(RecipeMainIngredient)
-admin.site.register(Ingredient)
 admin.site.register(PrepTime)
 admin.site.register(RecipePrepTimes)
 admin.site.register(Cuisine)
