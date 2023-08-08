a8e95909cccd9f9b7906a536d875311a7995ce2c
mustafa <mustafamcm9@gmail.com>
2023-08-08 11:57:00 +0300

    update settings

--- RecipeShare/settings.py
+++ RecipeShare/settings.py
@@ -125,6 +125,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
+STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
